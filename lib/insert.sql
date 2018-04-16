32c3fdf625e612ad7f83f82392d5b68ad1dbb6f4
Yina Zhu <yinagit@gmail.com>
2018-04-16 23:48:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO series (id, title, author_id, subgenre_id) 
-VALUES (1, "A Song of Ice and Fire", 1, 1), 
+INSERT INTO series (id, title, author_id, subgenre_id)
+VALUES (1, "A Song of Ice and Fire", 1, 1),
        (2, "Second Series", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
