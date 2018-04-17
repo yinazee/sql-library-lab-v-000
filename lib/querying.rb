317ef2e9ba5c7fbc22a0d159e36db957c269a9db
Yina Zhu <yinagit@gmail.com>
2018-04-17 01:03:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title
+  "SELECT series.title
     FROM series
     JOIN books
     ON books.series_id = series.id
