6734db79d3c9f432538fde33c0279af4645b58ac
Yina Zhu <yinagit@gmail.com>
2018-04-17 00:38:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT value, count
+    FROM characters
+    GROUP BY species
+    ORDER BY COUNT(species) DESC
+    LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
