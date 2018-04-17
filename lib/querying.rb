fe989ade9e3b22f01d948e13caa9cf69b4b9a0a7
Yina Zhu <yinagit@gmail.com>
2018-04-17 00:50:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT * FROM series"
+  "SELECT authors.name, subgenres.name
+    FROM authors
+    JOIN series
+    ON series.author_id = author.id
+    JOIN subgenres
+    ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
