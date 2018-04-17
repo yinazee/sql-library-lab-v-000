761f99913ec029dbf3c54b8b57d922128a1affc0
Yina Zhu <yinagit@gmail.com>
2018-04-17 00:34:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,12 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
+  "SELECT title, year
     FROM books
     WHERE series_id = 1
     ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+    FROM characters
+    WHERE characters."
 end
 
 
