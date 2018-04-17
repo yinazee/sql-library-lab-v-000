140c0f5847fa9dd77a694a32d1de7d9cf8afa9a2
Yina Zhu <yinagit@gmail.com>
2018-04-17 00:41:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, COUNT(*)
+  "SELECT species, COUNT(*)
     FROM characters
     GROUP BY species
     ORDER BY COUNT(species) DESC
